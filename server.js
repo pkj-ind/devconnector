8d26b8037d1db54e72cb2222fe99b39168c5f29b
Pramod Jaiswal <pramodix@gmail.com>
2019-10-11 10:36:29 +0530

    routes defined

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ app.get('/',(req,res)=>{
     console.log(req.params)
     res.send("Api Running !!")
 })
+
+//Define Routes
+app.use('/api/users',require('./routes/api/users'))
+app.use('/api/auth',require('./routes/api/auth'))
+app.use('/api/profile',require('./routes/api/profile'))
+app.use('/api/posts',require('./routes/api/posts'))
+
 app.listen(3000,()=>{
     console.log("Successfully Connected!!!")
 })
