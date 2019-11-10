355b696da68aa203da64d6bfd73422501984ebe2
Pramod Jaiswal <pramodix@gmail.com>
2019-11-10 18:29:43 +0530

    Protected Route for Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Alert from './components/layout/Alert'
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/routing/PrivateRoute'
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -32,6 +34,7 @@ function App() {
       <Switch>
         <Route exact path='/register' component={Register} />
         <Route exact path='/login' component={Login} />
+        <PrivateRoute exact path='/dashboard' component={Dashboard} />
       </Switch>
     </section>
     </Fragment>
